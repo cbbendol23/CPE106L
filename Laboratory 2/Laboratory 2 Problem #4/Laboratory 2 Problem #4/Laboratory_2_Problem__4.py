d729cdd8c21454a97025d75f86a6df4ced3566b0
Cherry Ben Jr. R. Bendol <cbbendol23@gmail.com>
2025-01-01 14:42:26 +0800

    new commit

--- Laboratory 2/Laboratory 2 Problem #4/Laboratory 2 Problem #4/Laboratory_2_Problem__4.py
+++ Laboratory 2/Laboratory 2 Problem #4/Laboratory 2 Problem #4/Laboratory_2_Problem__4.py
@@ -3,7 +3,8 @@
   LAB 02
   INLAB 04
   
-  Write a program in the file unique.py that inputs a text file. The program should print the unique words in the file in alphabetical order. (LO: 5.1)
+  Write a program in the file unique.py that inputs a text file. 
+  The program should print the unique words in the file in alphabetical order. (LO: 5.1)
 
   Credits: Fundamentals of Python: First Programs Chapter 05
 """
